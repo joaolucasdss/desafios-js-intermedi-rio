60ee656f9b34fc5ebfe684032f7abbea74ccfeb5
joaolucasdss <joaolucas7371@gmail.com>
2022-02-22 11:57:27 -0300

    addind details

--- Desafio 1/index.js
+++ Desafio 1/index.js
@@ -1,4 +1,6 @@
 let slidePosition = 0;
+let setaEsquerda = document.getElementById('gliderPrev')
+let setaDireita = document.getElementById('gliderNext')
 const slides = document.getElementsByClassName('slide');
 const totalSlides = slides.length;
 
@@ -21,20 +23,32 @@ function atualizarSlidePosition() {
 
 function avancarSlide() {
     if (slidePosition === totalSlides - 1) {
-        slidePosition = 0;
-    } else {
-        slidePosition++;
+        return
     }
+
+    if (slidePosition === totalSlides - 2) {
+        setaDireita.classList.remove('opacidade')
+    } else {
+        setaEsquerda.classList.add('opacidade')
+    }
+    
+    slidePosition++
 
     atualizarSlidePosition();
 }
 
 function retrocederSlide() {
     if (slidePosition === 0) {
-        slidePosition = totalSlides - 1;
-    } else {
-        slidePosition--;
+        return
     }
+
+    if (slidePosition === totalSlides - 3) {
+        setaEsquerda.classList.remove('opacidade')
+    } else {
+        setaDireita.classList.add('opacidade')
+    }
+    
+    slidePosition--
 
     atualizarSlidePosition();
 }
